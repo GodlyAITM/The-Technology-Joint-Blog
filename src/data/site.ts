a748a559395c58d4fb27674fb8d060533ba26a28
GodlyAITM <alabi43israel@gmail.com>
2026-06-28 01:10:45 +0100

    v0.1.2 - Build homepage publication framework

---
+++ src/data/site.ts
@@ -0,0 +1,7 @@
+export const site = {
+  name: "The Technology Joint",
+  tagline: "Technology Insights That Help You Work Smarter.",
+  founder: "Israel Alabi",
+  email: "israel.alabi.seo@gmail.com",
+  url: "https://thetechnologyjoint.com",
+};
